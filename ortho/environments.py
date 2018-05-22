9621f28cb321b360d9363338d8b353441b00bc0f
Ryan Bradley <bradleyrp@gmail.com>
2018-05-22 17:58:26 -0400

    developing environments

--- ortho/environments.py
+++ ortho/environments.py
@@ -6,11 +6,20 @@ import sys,os
 
 class Factory:
 	def __init__(self,*args,**kwargs):
-		self.where = kwargs.pop('where','./env')
-		if not self.env_exists(): print('environment %s does not exist'%self.where)
-		else: print('status','found environment at %s'%self.where)
-	def env_exists(self):
-		return os.path.isfile(self.where)
+		self.conf = conf # pylint: disable=undefined-variable
+		self.envs = self.conf.get('envs',{})
+		self.logs_space()
+		if not self.envs: raise Exception('no environments yet!')
+		for name,detail in self.envs.items(): self.validate(name,detail)
+	def logs_space(self):
+		"""Logs are always written to the same place."""
+		if not os.path.isdir('logs'): os.mkdir('logs')
+	def validate(self,name,detail):
+		"""Check or create an environment."""
+		where = detail.pop('where')
+		sources = detail.pop('sources')
+		style = detail.pop('style')
+		install_commands = detail.pop('install_commands',install_defaults[style])
+		raise Exception('dev')
 
-def manage(*args,**kwargs): 
-	Factory(*args,**kwargs)
+def env(*args,**kwargs): Factory(*args,**kwargs)
