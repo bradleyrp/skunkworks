4157223b435ca0046f9083414733ba4f682e24b5
Ryan Bradley <bradleyrp@gmail.com>
2018-05-30 09:59:43 -0400

    added scroll option for bash calls

--- ortho/bash.py
+++ ortho/bash.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import os,subprocess
+import os,sys,subprocess
 
 def command_check(command):
 	"""Run a command and see if it completes with returncode zero."""
@@ -15,7 +15,16 @@ def command_check(command):
 		print('warning','caught exception on command_check: %s'%e)
 		return False
 
-def bash(command,log=None,cwd=None,inpipe=None,show=False):
+import Queue,threading
+
+def reader(pipe,queue):
+	try:
+		with pipe:
+			for line in iter(pipe.readline, b''):
+				queue.put((pipe, line))
+	finally: queue.put(None)
+
+def bash(command,log=None,cwd=None,inpipe=None,show=False,scroll=True):
 	"""
 	Run a bash command.
 	Development note: tee functionality would be useful however you cannot use pipes with subprocess here.
@@ -32,6 +41,23 @@ def bash(command,log=None,cwd=None,inpipe=None,show=False):
 		kwargs = dict(cwd=cwd,shell=True,executable='/bin/bash')
 		proc = subprocess.Popen(command,**kwargs)
 		stdout,stderr = proc.communicate()
+	elif log and scroll:
+		# via: https://stackoverflow.com/questions/31833897/python-read-from-subprocess-stdout-and-stderr-separately-while-preserving-order
+		proc = subprocess.Popen(command,cwd=cwd,shell=True,executable='/bin/bash',
+			stdout=subprocess.PIPE,stderr=subprocess.PIPE,bufsize=1)
+		if False:
+			with open(log,'ab') as file:
+				for line in p.stdout: # b'\n'-separated lines
+					sys.stdout.buffer.write(line) # pass bytes
+					file.write(line)
+		qu = Queue.Queue()
+		threading.Thread(target=reader,args=[proc.stdout,qu]).start()
+		threading.Thread(target=reader,args=[proc.stderr,qu]).start()
+		with open(log,'ab') as fp:
+			for _ in range(2):
+				for _,line in iter(qu.get,None):
+					print(u'\r'+'[TAIL]','%s: %s'%(log,line),end='')
+					fp.write(line)
 	else:
 		output = open(log,'w')
 		kwargs = dict(cwd=cwd,shell=True,executable='/bin/bash',
@@ -40,7 +66,7 @@ def bash(command,log=None,cwd=None,inpipe=None,show=False):
 		proc = subprocess.Popen(command,**kwargs)
 		if not inpipe: stdout,stderr = proc.communicate()
 		else: stdout,stderr = proc.communicate(input=inpipe)
-	if stderr: 
+	if not scroll and stderr: 
 		print('error','stdout: %s'%stdout)
 		print('error','stderr: %s'%stderr)
 		raise Exception('bash returned error state')
@@ -50,4 +76,4 @@ def bash(command,log=None,cwd=None,inpipe=None,show=False):
 			extra = '\n'.join([i for i in [stdout,stderr] if i])
 			raise Exception('bash error with returncode %d. stdout: "%s"\nstderr: "%s"'%(proc.returncode,
 				stdout,stderr))
-	return {'stdout':stdout,'stderr':stderr}
+	return None if scroll else {'stdout':stdout,'stderr':stderr}
